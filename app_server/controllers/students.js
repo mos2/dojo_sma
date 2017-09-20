f7036766ee5e094acb544df5aec79aaae80ae5ee
Michael O'Sullivan <mjjos1@gmail.com>
2017-09-20 22:35:59 +0100

    Added rapid prototype page for adding new students. Updated student list to move student data out of view and into controller. Updated view to render data from controller programmatically.

--- app_server/controllers/students.js
+++ app_server/controllers/students.js
@@ -1,6 +1,36 @@
 /* GET students page */
 const studentsList = function(req, res) {
-    res.render('student-list', { title: 'IBM Cork CoderDojo - Student Management' });
+    res.render('student-list',
+        { title: 'IBM Cork CoderDojo - Student Management: Student List',
+            pageHeader:
+                {
+                    title: 'Student List',
+                    description: 'Browse through all the students registered for the CoderDojo'
+            },
+            students:
+            [
+                {
+                    name: "Shane Williams",
+                    email: "shane@anemail.com",
+                    mentor: false,
+                    current_class: "Python",
+                    current_role: "Ninja",
+                    last_belt: "White",
+                    last_attended: "16th September",
+                    last_class: "Web Dev"
+                },
+                {
+                    name: "David Hurley",
+                    email: "djm@gmail.com",
+                    mentor: true,
+                    current_class: "Python",
+                    current_role: "Mentor",
+                    last_belt: "Black",
+                    last_attended: "16th September",
+                    last_class: "Comp Sci"
+                }
+            ]
+        });
 };
 
 const studentInfo = function(req, res) {
@@ -8,7 +38,7 @@ const studentInfo = function(req, res) {
 };
 
 const addStudent = function(req, res) {
-    res.render('index', { title: 'IBM Cork CoderDojo - Student Management' });
+    res.render('add-student-form', { title: 'IBM Cork CoderDojo - Student Management' });
 };
 
 module.exports = {
